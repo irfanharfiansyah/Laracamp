7f6ec223e9d7300414802e515f461cfe6160c1cd
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2021-11-08 15:26:44 +0700

    Add Success Checkout Page

---
+++ resources/views/layouts/success-page.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+@section('title', 'Checkout Succes')
+@include('components.navbar')
+
+<section class="success-page">
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-lg-12 col-12 text-center">
+                <img src="/icon/ill_checkout.png" alt="" class="img-fluid" width="380">
+                <p>WHAT A DAY!</p>
+                <p>Berhasil Checkout</p>
+                <a href="#" class="btn btn-primary mb-5">My Dashboard</a>
+            </div>
+        </div>
+    </div>
+</section>
